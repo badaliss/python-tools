44945a2dcea5a44033b1cade1674427d7f80702f
ms Ʌlice <135126758+ke5sa@users.noreply.github.com>
2023-06-13 10:20:18 +0300

    Update network_scan.py

--- network_scan.py
+++ network_scan.py
@@ -6,7 +6,6 @@ import scapy.all as scapy
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
-    # scapy.ls(scapy.ARP()) - to see the fields for ip and mac
     arp_request_broadcast = broadcast/arp_request
     answered_lst = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
